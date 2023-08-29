4be9fb3b787514ecba779e3e4610480d4cdf45b2
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-08-30 00:57:45 +0530

    checkout page logic

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+import datetime
 
 # Create your views here.)
 
@@ -88,5 +89,29 @@ def updateItem(request):
         return JsonResponse({'status': 'error', 'error': str(e)}, status=500)
     
 def processOrder(request):
-    print('Data:', request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        
+        if order.shipping == True:
+            ShippingAddress.object.create (
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode']
+            )
+        
+    else:
+        print('user is not logged in')
     return JsonResponse('Payment complete', safe=False)
