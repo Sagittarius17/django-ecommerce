d55930ee2f0ca6e84642b856805d51f2743b55b3
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-09-10 22:57:26 +0530

    db change to postgresql

--- djangoEcomm/settings.py
+++ djangoEcomm/settings.py
@@ -85,15 +85,21 @@ WSGI_APPLICATION = 'djangoEcomm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        ### SQLite3
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        
-        ### mongodb
-        'ENGINE': 'djongo',
-        'NAME': BASE_DIR / 'ecom.db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-ecom',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
