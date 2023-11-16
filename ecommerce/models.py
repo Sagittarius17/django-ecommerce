b0f8dfcabf0d34b9c9e2513c360a379276ad78cf
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-11-16 22:27:40 +0530

    login bug

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -9,19 +9,8 @@ class Customer(models.Model):
     phone_number = models.CharField(unique=True, max_length=20, null=True)
     password = models.CharField(max_length=255, null=True)
     
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'email', 'phone_number', 'password']
-    
     def __str__(self):
         return self.username
-
-# class Customer(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-#     name = models.CharField(max_length=200, null=True)
-#     email = models.CharField(max_length=200, null=True)
-    
-#     def __str__(self):
-#         return self.name
     
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
